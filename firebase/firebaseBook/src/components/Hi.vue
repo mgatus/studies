113331f57bef6999b84790e43043c64cdf273860
Solid Snake <Solid Snake>
2017-08-14 07:38:50 +0800

    fire and blood

---
+++ firebase/firebaseBook/src/components/Hi.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hiCLass">
+    Hi There
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
